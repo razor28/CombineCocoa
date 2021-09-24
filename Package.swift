64042ef1f5d65e27a4ec08b826628d45e850d236
Dastan Tashimbetov <arabesku@gmail.com>
2021-09-24 02:41:33 -0700

    Update platform version

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "CombineCocoa",
-    platforms: [.iOS(.v10)],
+    platforms: [.iOS(.v12)],
     products: [
         .library(name: "CombineCocoa", targets: ["CombineCocoa"]),
     ],
